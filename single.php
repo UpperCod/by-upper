63e7754cb3e5ac0110d95980efe9771aaf313e10
Matias Trujillo <mtrujillo@outlook.cl>
2018-08-24 17:18:02 -0300

    update

--- single.php
+++ single.php
@@ -7,6 +7,6 @@ $context["page"] = Timber::get_post();
 
 Timber::render([
     "views/pages/single-{$context["page"]->type}.twig",
-    "views/pages/single.twig"
+    "views/pages/single.twig",
     "views/pages/404.twig"
 ], $context);
