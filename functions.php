7b82aea39c7a7dae617a0708a344a3a4554d96af
Matias Trujillo <mtrujillo@outlook.cl>
2018-10-08 21:31:16 -0300

    update default function.php

--- functions.php
+++ functions.php
@@ -15,11 +15,11 @@ if (!function_exists("dd")) {
         die;
     }
 }
-/*
+
 add_theme_support( 'post-formats' );
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'menus' );
-
+/*
 if( function_exists('acf_add_options_page') ){
     acf_add_options_page([
         'page_title'=>'Sitio',
