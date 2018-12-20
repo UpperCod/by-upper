5cbff4d0389ab406371f68c3869accdce0832504
Matias Trujillo <mtrujillo@outlook.cl>
2018-12-20 12:54:26 -0300

    update

--- functions.php
+++ functions.php
@@ -1,6 +1,4 @@
 <?php
-
-require_once __DIR__."/script/custom-twig.php";
 /*
  * Die and Dump method
  */
@@ -15,6 +13,18 @@ if (!function_exists("dd")) {
         die;
     }
 }
+
+if ( ! class_exists( 'Timber' ) ) {
+	add_action( 'admin_notices', function() {
+		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+	});
+	add_filter('template_include', function( $template ) {
+		return get_stylesheet_directory() . '/views/no-timber.html';
+	});
+	return;
+}
+
+require_once __DIR__."/script/custom-twig.php";
 
 add_theme_support( 'post-formats' );
 add_theme_support( 'post-thumbnails' );
