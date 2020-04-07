2c210ff46536024c11fdaebe41e062cd05359ae8
UpperCod <mtrujillo@outlook.cl>
2020-04-06 23:07:29 -0400

    update js

--- src/utils/scroll-observer.js
+++ src/utils/scroll-observer.js
@@ -1,18 +1,41 @@
-export default function() {
-	return new IntersectionObserver(
-		entries =>
-			entries.forEach(({ isIntersecting, target }) => {
-				target[isIntersecting ? "setAttribute" : "removeAttribute"](
-					"intercepted",
-					""
-				);
-				if (target.dataset.src) {
-					target.setAttribute("src", target.dataset.src);
-				}
-			}),
+const OBSERVER = Symbol("scroll.observer");
+export default function () {
+  let intersection = new IntersectionObserver(
+    (entries) =>
+      entries.forEach((event) => {
+        event.target[OBSERVER](event);
+      }),
+    {
+      rootMargin: "200px 0px",
+    }
+  );
 
-		{
-			rootMargin: "200px 0px"
-		}
-	);
+  return (element) => {
+    let node = element;
+    let dataset = element.dataset;
+    switch (dataset.scrollBefore) {
+      case "nextSibling":
+        node = element.nextSibling;
+        break;
+    }
+    node[OBSERVER] = ({ isIntersecting }) => {
+      let state = isIntersecting ? "intercepted" : "not-intercepted";
+      dataset.scrollBefore = state;
+      if (dataset.src && isIntersecting) {
+        if (!element.hasAttribute("src")) {
+          if (element.localName == "video") {
+            element.addEventListener("canplay", () => {
+              setTimeout(() => {
+                element.style.opacity = 1;
+              }, 100);
+            });
+          }
+          element.setAttribute("src", dataset.src);
+        }
+        delete dataset.src;
+      }
+    };
+
+    intersection.observe(node);
+  };
 }
